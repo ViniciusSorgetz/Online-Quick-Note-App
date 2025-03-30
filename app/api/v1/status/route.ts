dfa8a654b0ebd03fdd86fe24164a563cf2cc24b4
Rexzinhoo <viniciussorgetzalves@gmail.com>
2025-03-30 15:58:25 -0300

    ci: Add `eslint` `workflow`

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const esquecida = "teste";
   return NextResponse.json(
     { message: "hello" },
     {
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,5 +1,5 @@
 test("API first test", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
-  expect(responseBody).toEqual({ message: "hello" });
+          expect(responseBody).toEqual({ message: "hello" });
 });
