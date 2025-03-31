79146ce5e938b0418dc82bc02ec2914fa7d48269
Rexzinhoo <viniciussorgetzalves@gmail.com>
2025-03-31 08:37:47 -0300

    ci: Add `commitlint` to `lint.yaml` workflow

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,10 @@
-import type { UserConfig } from "@commitlint/types"; 
-import { RuleConfigSeverity } from "@commitlint/types"; 
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
 
 const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "subject-case": [RuleConfigSeverity.Error, "always", ["sentence-case"]], 
+    "subject-case": [RuleConfigSeverity.Error, "always", ["sentence-case"]],
   },
 };
 
